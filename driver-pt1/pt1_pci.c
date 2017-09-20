ffeec6a1b643553f502275437f2fa0e178313544
shirow-github <shirowxxx@gmail.com>
2017-09-20 13:54:09 +0900

    Linux Kernel 4.12 以降への対応
    参照：Support 4.12 or above. by toshipp Pull Request #10
    m-tsudo/pt3 https://github.com/m-tsudo/pt3/pull/10

--- driver-pt1/pt1_pci.c
+++ driver-pt1/pt1_pci.c
@@ -11,13 +11,17 @@
 #include <linux/interrupt.h>
 #include <linux/version.h>
 #include <linux/mutex.h>
+#include <linux/uaccess.h>
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3,4,0)
 #include <asm/system.h>
 #endif
 #include <asm/io.h>
 #include <asm/irq.h>
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4,12,0)
 #include <asm/uaccess.h>
+#endif
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,23)
 #include <linux/freezer.h>
